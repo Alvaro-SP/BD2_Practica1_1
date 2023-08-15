2b359fa77ac47f525c6312fbf0f0bbd23cdfc855
Erwin_14k <82339728+Erwin14k@users.noreply.github.com>
2023-08-15 13:30:32 -0600

    [ADD] Function 5 (Func_logger) prototype, pending testing

--- db/functions.sql
+++ db/functions.sql
@@ -1,1 +1,29 @@
 -- Functions
+
+
+--Function5 "Func_logger"
+-- Crear la función
+CREATE FUNCTION Func_logger()
+RETURNS @Resultados TABLE (
+    Id INT,
+    Date DATETIME2(7),
+    Description NVARCHAR(MAX)
+)
+AS
+BEGIN
+  BEGIN TRY
+    BEGIN TRANSACTION;
+      INSERT INTO @Resultados
+      SELECT Id, Date, Description
+      FROM practica1.HistoryLog;
+      COMMIT TRANSACTION;
+  END TRY
+  BEGIN CATCH
+    IF @@TRANCOUNT > 0
+      ROLLBACK TRANSACTION;
+      -- En caso de error, mensaje de error
+    DECLARE @ErrorMessage NVARCHAR(4000) = 'Error, no se pudieron obtener los resultados de la tabla HistoryLog';
+    THROW 50001, @ErrorMessage, 1;
+  END CATCH;
+  RETURN;
+END;
