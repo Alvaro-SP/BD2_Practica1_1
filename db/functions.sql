dc2fed384f9751704fa30175dce440e54a807efa
Erwin_14k <82339728+Erwin14k@users.noreply.github.com>
2023-08-17 17:13:51 -0600

    [FIX] Function 4 update

--- db/functions.sql
+++ db/functions.sql
@@ -1,35 +1,15 @@
 -- Functions
 
-
 --Function 4 "Func_logger"
--- Crear la función
 CREATE FUNCTION Func_logger()
-RETURNS @Resultados TABLE (
-    Id INT,
-    Date DATETIME2(7),
-    Description NVARCHAR(MAX)
-)
+RETURNS TABLE
 AS
-BEGIN
-  BEGIN TRY
-    BEGIN TRANSACTION;
-      INSERT INTO @Resultados
-      SELECT Id, Date, Description
-      FROM practica1.HistoryLog;
-      COMMIT TRANSACTION;
-  END TRY
-  BEGIN CATCH
-    IF @@TRANCOUNT > 0
-      ROLLBACK TRANSACTION;
-      -- En caso de error, mensaje de error
-    DECLARE @ErrorMessage NVARCHAR(4000) = 'Error, no se pudieron obtener los resultados de la tabla HistoryLog';
-    THROW 50001, @ErrorMessage, 1;
-  END CATCH;
-  RETURN;
-END;
+RETURN(
+  SELECT Id, Date, Description
+  FROM practica1.HistoryLog
+);
 
 -- Function 5 "Func_usuarios"
--- Crear la función
 CREATE FUNCTION Func_usuarios(@UserId uniqueidentifier)
 RETURNS @Resultado TABLE (
   Firstname NVARCHAR(MAX),
