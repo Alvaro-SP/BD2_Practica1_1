0205d7bb1934991f48cb728659788b20730f1c1a
Erwin_14k <82339728+Erwin14k@users.noreply.github.com>
2023-08-17 17:21:36 -0600

    [FIX] Function 5 update

--- db/functions.sql
+++ db/functions.sql
@@ -2,16 +2,21 @@
 
 --Function 4 "Func_logger"
 CREATE FUNCTION Func_logger()
-RETURNS TABLE
+RETURNS @results TABLE (
+    Id INT,
+    Date DATETIME2(7),
+    Description NVARCHAR(MAX)
+)
 AS
-RETURN(
-  SELECT Id, Date, Description
-  FROM practica1.HistoryLog
-);
+BEGIN
+    SELECT *
+    FROM historyLog;
+    RETURN;
+END;
 
 -- Function 5 "Func_usuarios"
 CREATE FUNCTION Func_usuarios(@UserId uniqueidentifier)
-RETURNS @Resultado TABLE (
+RETURNS @results TABLE (
   Firstname NVARCHAR(MAX),
   Lastname NVARCHAR(MAX),
   Email NVARCHAR(MAX),
@@ -21,23 +26,11 @@ RETURNS @Resultado TABLE (
 )
 AS
 BEGIN
-  BEGIN TRY
-    BEGIN TRANSACTION;
-      INSERT INTO @Resultado (Firstname, Lastname, Email, DateOfBirth, Credits, RoleName)
-      SELECT u.Firstname, u.Lastname, u.Email, u.DateOfBirth, uc.Credits, r.RoleName
-      FROM practica1.Usuarios u
-      INNER JOIN practica1.UsuarioRole ur ON u.Id = ur.UserId
-      INNER JOIN practica1.Roles r ON ur.RoleId = r.Id
-      WHERE u.Id = @UserId;
-      COMMIT TRANSACTION;
-  END TRY
-  BEGIN CATCH
-    IF @@TRANCOUNT > 0
-      ROLLBACK TRANSACTION;
-    -- Manejo de errores, mensaje personalizado
-    DECLARE @ErrorMessage NVARCHAR(4000) = 'Error, no se pudieron obtener los datos del estudiante buscado';
-    THROW 50001, @ErrorMessage, 1;
-    END CATCH;
+    SELECT u.Firstname, u.Lastname, u.Email, u.DateOfBirth, uc.Credits, r.RoleName
+    FROM practica1.Usuarios u
+    INNER JOIN practica1.UsuarioRole ur ON u.Id = ur.UserId
+    INNER JOIN practica1.Roles r ON ur.RoleId = r.Id
+    WHERE u.Id = @UserId;
     RETURN;
 END;
 
