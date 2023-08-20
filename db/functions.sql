6618ebc09dd1a04af5a6d31f1c7fc3efd01fd36e
SergieArizandieta <3000523830101@ingenieria.usac.edu.gt>
2023-08-19 23:01:28 -0600

    Function 1 n 2

--- db/functions.sql
+++ db/functions.sql
@@ -1,8 +1,8 @@
--- Functions
+
+--Function 1 "Func_credits"
 USE BD2
 GO
 
---Function 1 "Func_credits"
 CREATE FUNCTION GetStudentsInCourse
 (
     @CourseCodCourse INT
@@ -24,7 +24,30 @@ RETURN
         ca.CourseCodCourse = @CourseCodCourse
 );
 
---Function 2 "Func_credits"
+--Function 2 "Func_tutor_course"
+USE BD2
+GO
+
+CREATE FUNCTION GetCoursesForTutor
+(
+    @TutorId UNIQUEIDENTIFIER
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT
+        ct.Id AS CourseTutorId,
+        c.CodCourse,
+        c.Name AS CourseName,
+        c.CreditsRequired
+    FROM
+        practica1.CourseTutor ct
+    INNER JOIN
+        practica1.Course c ON ct.CourseCodCourse = c.CodCourse
+    WHERE
+        ct.TutorId = @TutorId
+);
 
 
 --Function 4 "Func_logger"
