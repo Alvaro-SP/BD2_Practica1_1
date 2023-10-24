73ce7ddd1d00e87e0dbafc9e57ca5f48fda90bb2
Alvaro-SP <3034161730108@ingenieria.usac.edu.gt>
2023-10-23 19:31:56 -0600

    added consulta general

--- proyectofase2/consultas.mongodb.js
+++ proyectofase2/consultas.mongodb.js
@@ -16,27 +16,125 @@ use('mongodbVSCodePlaygroundDB');
 // mongoimport --uri "mongodb+srv://3034161730108:dqUeDQxkCYEQDucE@fase2grupo1mongo.qzzcnmt.mongodb.net/" --file 'E:\U
 // sers\PC\Downloads\CSV_fase2\rich_games_optimized.json' -d mongodbVSCodePlaygroundDB -c games --jsonArray --drop
 
-use('mongodbVSCodePlaygroundDB');
-// * CONSULTA 1
-const c1 = db.getCollection('games').find({}, //buscar todos los documentos en tu colección.
-    { name: 1, "platforms.name": 1, rating: 1, "genres.name": 1 })//campos a mostrar
-    .sort({ rating: -1 }) //ordenar los juegos en orden descendente según la valoración (Rating). Esto significa que los juegos con la valoración más alta aparecerán primero.
-    .limit(100); //  limitar los resultados a los 100 primeros juegos en función de su valoración.
 
-c1.forEach(function (item) {
-    console.log("Name:", item.name);
-    if (item.genres) {
-        console.log("Genres:");
-        for (const genre of item.genres) {
-            console.log("  -" + genre.name);
-        }
+// * CONSULTA 1
+// use('mongodbVSCodePlaygroundDB');
+// const c1 = db.getCollection('games').find({}, //buscar todos los documentos en tu colección.
+//     { name: 1, "platforms.name": 1, rating: 1, "genres.name": 1 })//campos a mostrar
+//     .sort({ rating: -1 }) //ordenar los juegos en orden descendente según la valoración (Rating). Esto significa que los juegos con la valoración más alta aparecerán primero.
+//     .limit(100); //  limitar los resultados a los 100 primeros juegos en función de su valoración.
+
+// c1.forEach(function (item) {
+//     console.log("Name:", item.name);
+//     if (item.genres) {
+//         console.log("Genres:");
+//         for (const genre of item.genres) {
+//             console.log("  -" + genre.name);
+//         }
+//     }
+//     if (item.platforms) {
+//         console.log("Platforms:");
+//         for (const platform of item.platforms) {
+//             console.log("  -" + platform.name);
+//         }
+//     }
+//     console.log("Rating:", item.rating);
+//     console.log("----------------------");
+// });
+
+
+// * CONSULTA GENERAL
+use('mongodbVSCodePlaygroundDB');
+const searchGameByName = (name) => {
+    const regex = new RegExp(name, 'i'); // 'i' para que la búsqueda no distinga entre mayúsculas y minúsculas
+    const query = { name: { $regex: regex } };
+
+    const results = db.getCollection('games').find(query).limit(100).toArray();
+
+    return results;
+};
+
+// Llamar a la función para buscar juegos por nombre
+const searchResults = searchGameByName('Super'); // Cambia 'Super Mario' por el nombre que desees buscar
+function convertirFechaUnixATexto(fechaInt) {
+    // Convierte la fecha INT a DATETIME
+    const fecha = new Date(fechaInt * 1000); // Multiplicamos por 1000 para convertir segundos a milisegundos
+
+    // Formatea la fecha en un formato legible (ISO 8601)
+    const fechaTexto = fecha.toISOString(); // Esto da formato en "YYYY-MM-DDTHH:MM:SS.sssZ"
+
+    return fechaTexto;
+}
+// Imprimir los resultados
+searchResults.forEach(function (item) {
+    console.log('Name:' + item.name);
+    if (item.first_release_date) {
+        console.log('First release date:', convertirFechaUnixATexto(item.first_release_date));
     }
-    if (item.platforms) {
-        console.log("Platforms:");
-        for (const platform of item.platforms) {
-            console.log("  -" + platform.name);
-        }
+    if (Array.isArray(item.alternative_names)) {
+        console.log('alternative_names:', item.alternative_names.map(platform => '\t - ' + platform.name + '--' + platform.comment).join(', '));
     }
-    console.log("Rating:", item.rating);
-    console.log("----------------------");
+    console.log('Genres:', item.genres && item.genres.length > 0 ? item.genres.map(genre => '\t - ' + genre.name).join(', ') : 'No genres available');
+    if (Array.isArray(item.platforms)) {
+        console.log('Platforms:', item.platforms.map(platform => '\t - ' + platform.name).join(', '));
+    }
+    if (item.rating) {
+        console.log('Rating:' + item.rating);
+    }
+    if (item.aggregated_rating) {
+        console.log('aggregated_rating:' + item.aggregated_rating);
+    }
+    if (item.rating_count) {
+        console.log('rating_count:' + item.rating_count);
+    }
+    if (item.summary) {
+        console.log('summary:' + item.summary);
+    }
+    if (Array.isArray(item.release_dates)) {
+        console.log('release_dates:', item.release_dates.map(release => '\t - ' + release.human).join(', '));
+    }
+    if (Array.isArray(item.involved_companies)) {
+        console.log('involved_companies:', item.involved_companies.map(r => {
+            if (Array.isArray(r.company)) {
+                return r.company.map(w => w.name).join(', ');
+            }
+        }));
+    }
+    if (Array.isArray(item.genres)) {
+        console.log('genres:', item.genres.map(g => '\t - ' + g.name).join(', '));
+    }
+    if (Array.isArray(item.themes)) {
+        console.log('themes:', item.themes.map(g => '\t - ' + g.name).join(', '));
+    }
+    if (Array.isArray(item.game_modes)) {
+        console.log('game_modes:', item.game_modes.map(g => '\t - ' + g.name).join(', '));
+    }
+    if (Array.isArray(item.player_perspectives)) {
+        console.log('player_perspectives:', item.player_perspectives.map(g => '\t - ' + g.name).join(', '));
+    }
+    if (Array.isArray(item.collection)) {
+        console.log('collection:', item.collection.map(g => '\t - ' + g.name).join(', '));
+    }
+    if (item.storyline) {
+        console.log('storyline:', item.storyline);
+    }
+    if (Array.isArray(item.franchises)) {
+        console.log('franchises:', item.franchises.map(g => '\t - ' + g.name).join(', '));
+    }
+    if (Array.isArray(item.game_engines)) {
+        console.log('game_engines:', item.game_engines.map(g => '\t - ' + g.name).join(', '));
+    }
+    if (Array.isArray(item.game_localizations)) {
+        console.log('game_localizations:', item.game_localizations.map(g => '\t - ' + g.name).join(', '));
+    }
+    if (Array.isArray(item.language_supports)) {
+        console.log('game_localizations:', item.language_supports.map(r => {
+            if (Array.isArray(r.language)) {
+                return r.language.map(w => w.name + " " + w.locale).join(', ');
+            }
+        }));
+    }
+
+    console.log('----------------------');
 });
+
